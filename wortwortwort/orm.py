fc96a521b71f3e758eb475d52cddb8b0eaf46fe9
Favio Manriquez Leon <favio@favrik.com>
2013-03-30 18:17:30 -0700

    Add Manager and App classes to wortwortwort.orm

--- wortwortwort/orm.py
+++ wortwortwort/orm.py
@@ -2,12 +2,37 @@ from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref, sessionmaker
 
+class Manager:
+    def __init__(self):
+        self.engine = create_engine('sqlite:///db.sqlite', echo=True)
+        Session = sessionmaker(bind=self.engine)
+        self.session = Session()
+        self.setup()
+
+    def setup(self):
+        if (not self.engine.dialect.has_table(
+            self.engine.connect(), App.__tablename__)):
+
+            Base.metadata.create_all(self.engine)
+            self.session.add_all(
+                [ProjectType(name='CodebaseHQ'), ProjectType(name='Custom')])
+            self.session.commit()
+
+    def project_types(self):
+        return self.session.query(ProjectType).all()
+ 
+
+# Tables
 class Base(object):
     id = Column(Integer, primary_key=True)
     name = Column(String(250))
 
 Base = declarative_base(cls=Base)
 
+
+class App(Base):
+    __tablename__ = 'wortwortwort'
+    value = Column(String(250))
 
 class Project(Base):
     __tablename__ = 'projects' 
@@ -19,16 +44,14 @@ class Project(Base):
     config = relationship('Config')
 
     def __repr__(self):
-        return "<Project('%s')>" % (self.name)
+       return "<Project('%s')>" % (self.name)
 
 class ProjectType(Base):
     __tablename__ = 'project_types'
 
-    def __init__(self, name):
-        self.name = name
-
     def __repr__(self):
         return "<ProjectType('%s')>" % (self.name)
+
 
 class Config(Base):
     __tablename__ = 'project_config'
@@ -48,15 +71,5 @@ class Task(Base):
     def __repr__(self):
         return "<Task('%s', '%s')>" % (self.name, self.ticket)
 
-engine = create_engine('sqlite:///db.sqlite', echo=True)
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-
-# Setup the initial db data
-session = Session()
-session.add_all([
-    ProjectType('CodebaseHQ'),
-    ProjectType('Custom')])
-session.commit()
 
 
